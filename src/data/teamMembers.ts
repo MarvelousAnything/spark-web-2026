6856f72379db7848a4191306835910dad7cf1771
Owen Voskuhl Hayes <owen.v.hayes@gmail.com>
2026-02-03 21:59:39 -0500

    image optimizations

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -1,186 +1,49 @@
+import Naga from "../assets/headshots/naga.webp";
+import Kaito from "../assets/headshots/kaito.webp";
+import Kaustubha from "../assets/headshots/kaustubha.webp";
+import Flori from "../assets/headshots/flori.webp";
+import Nathan from "../assets/headshots/nathan.webp";
+import Eman from "../assets/headshots/eman.webp";
+import Niharika from "../assets/headshots/niharika.webp";
+import Huda from "../assets/headshots/huda.webp";
+import Mina from "../assets/headshots/mina.webp";
+import Sena from "../assets/headshots/sena.webp";
+import Gerard from "../assets/headshots/gerard.webp";
+import Mehtab from "../assets/headshots/mehtab.webp";
+import Stanley from "../assets/headshots/stanley.webp";
+import Zaina from "../assets/headshots/zaina.webp";
+import Kavya from "../assets/headshots/kavya.webp";
+import Rajvi from "../assets/headshots/rajvi.webp";
+import MJ from "../assets/headshots/mj.webp";
+import Adithya from "../assets/headshots/adithya.webp";
+import Rasleen from "../assets/headshots/rasleen.webp";
+import Dominic from "../assets/headshots/dominic.webp";
+import Josephine from "../assets/headshots/josephine.webp";
+import Daniel from "../assets/headshots/daniel.webp";
+import Fatima from "../assets/headshots/fatima.webp";
+
 export const teamMembers = [
-  {
-    name: "Naga Maddipudi",
-    title: "Director",
-    image: "/headshots/naga.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/naga-maddipudi/",
-  },
-  {
-    name: "Kaito Sekiya",
-    title: "Director",
-    image: "/headshots/kaito.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/kaitosekiya/",
-  },
-  {
-    name: "Kaustubha Medikundam",
-    title: "Communications Lead",
-    image: "/headshots/kaustubha.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/kmedikundam/",
-  },
-  {
-    name: "Florianne Che",
-    title: "Web Development Lead",
-    image: "/headshots/flori.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/florianne-che/",
-  },
-  {
-    name: "Nathan Trinh",
-    title: "Web Development Lead",
-    image: "/headshots/nathan.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/khoi-nguyen-trinh/",
-  },
-  {
-    name: "Eman Arsham",
-    title: "Outreach",
-    image: "/headshots/eman.webp",
-    year: "Senior",
-    major: "CS + Design",
-    linkedin: "https://www.linkedin.com/in/emanarsham/",
-  },
-  {
-    name: "Niharika Patil",
-    title: "Outreach",
-    image: "/headshots/niharika.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "http://linkedin.com/in/niharika-patil1468/",
-  },
-  {
-    name: "Huda Syed",
-    title: "Outreach",
-    image: "/headshots/huda.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/huda-syed17933/",
-  },
-  {
-    name: "Mina Pattanaik",
-    title: "Outreach",
-    image: "/headshots/mina.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/mina-pattanaik/",
-  },
-  {
-    name: "Sena Ozcan",
-    title: "Logistics",
-    image: "/headshots/sena.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/sena-ozcan/",
-  },
-  {
-    name: "Gerard Wilbert",
-    title: "Logistics",
-    image: "/headshots/gerard.webp",
-    year: "Senior",
-    major: "Mechanical Engineering",
-    linkedin: "https://www.linkedin.com/in/gerard-wilbert-a94636291/",
-  },
-  {
-    name: "Mehtab Kaur",
-    title: "Logistics",
-    image: "/headshots/mehtab.webp",
-    year: "Sophomore",
-    major: "Data Science",
-    linkedin: "https://www.linkedin.com/in/mehtabkaurr/",
-  },
-  {
-    name: "Stanley Huang",
-    title: "Logistics",
-    image: "/headshots/stanley.webp",
-    year: "Sophomore",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/stanley-huang12/",
-  },
-  {
-    name: "Zaina Khalil",
-    title: "Communications",
-    image: "/headshots/zaina.webp",
-    year: "Junior",
-    major: "Data Science",
-    linkedin: "https://www.linkedin.com/in/zainakhalil/",
-  },
-  {
-    name: "Kavya Patel",
-    title: "Communications",
-    image: "/headshots/kavya.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/kavya-patel-780314280/",
-  },
-  {
-    name: "Rajvi Shah",
-    title: "Experience",
-    image: "/headshots/rajvi.webp",
-    year: "Senior",
-    major: "CS + Linguistics",
-    linkedin: "https://www.linkedin.com/in/rajvi-shah12/",
-  },
-  {
-    name: "Rawisara Hameyotin",
-    title: "Experience",
-    image: "/headshots/mj.webp",
-    year: "Junior",
-    major: "Data Science",
-    linkedin: "https://www.linkedin.com/in/rawisarahameyotin/",
-  },
-  {
-    name: "Adithya Prasad",
-    title: "Experience",
-    image: "/headshots/adithya.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/adithya-prasad16/",
-  },
-  {
-    name: "Rasleen Dhaliwal",
-    title: "Experience",
-    image: "/headshots/rasleen.webp",
-    year: "Senior",
-    major: "CS + Design",
-    linkedin: "https://www.linkedin.com/in/rasleen-dhaliwal-/",
-  },
-  {
-    name: "Dominic Irla",
-    title: "Web Development",
-    image: "/headshots/dominic.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/dominic-irla/",
-  },
-  {
-    name: "Josephine Lee",
-    title: "Web Development",
-    image: "/headshots/josephine.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/josephine-b-l/",
-  },
-  {
-    name: "Daniel Barajas",
-    title: "Web Development",
-    image: "/headshots/daniel.webp",
-    year: "Senior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/danbarajas/",
-  },
-  {
-    name: "Fatima Rasheed",
-    title: "Web Development",
-    image: "/headshots/fatima.webp",
-    year: "Junior",
-    major: "Computer Science",
-    linkedin: "https://www.linkedin.com/in/fatima-rasheed-/",
-  },
-]
+  { name: "Naga Maddipudi", title: "Director", image: Naga, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/naga-maddipudi/" },
+  { name: "Kaito Sekiya", title: "Director", image: Kaito, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/kaitosekiya/" },
+  { name: "Kaustubha Medikundam", title: "Communications Lead", image: Kaustubha, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/kmedikundam/" },
+  { name: "Florianne Che", title: "Web Development Lead", image: Flori, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/florianne-che/" },
+  { name: "Nathan Trinh", title: "Web Development Lead", image: Nathan, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/khoi-nguyen-trinh/" },
+  { name: "Eman Arsham", title: "Outreach", image: Eman, year: "Senior", major: "CS + Design", linkedin: "https://www.linkedin.com/in/emanarsham/" },
+  { name: "Niharika Patil", title: "Outreach", image: Niharika, year: "Senior", major: "Computer Science", linkedin: "http://linkedin.com/in/niharika-patil1468/" },
+  { name: "Huda Syed", title: "Outreach", image: Huda, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/huda-syed17933/" },
+  { name: "Mina Pattanaik", title: "Outreach", image: Mina, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/mina-pattanaik/" },
+  { name: "Sena Ozcan", title: "Logistics", image: Sena, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/sena-ozcan/" },
+  { name: "Gerard Wilbert", title: "Logistics", image: Gerard, year: "Senior", major: "Mechanical Engineering", linkedin: "https://www.linkedin.com/in/gerard-wilbert-a94636291/" },
+  { name: "Mehtab Kaur", title: "Logistics", image: Mehtab, year: "Sophomore", major: "Data Science", linkedin: "https://www.linkedin.com/in/mehtabkaurr/" },
+  { name: "Stanley Huang", title: "Logistics", image: Stanley, year: "Sophomore", major: "Computer Science", linkedin: "https://www.linkedin.com/in/stanley-huang12/" },
+  { name: "Zaina Khalil", title: "Communications", image: Zaina, year: "Junior", major: "Data Science", linkedin: "https://www.linkedin.com/in/zainakhalil/" },
+  { name: "Kavya Patel", title: "Communications", image: Kavya, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/kavya-patel-780314280/" },
+  { name: "Rajvi Shah", title: "Experience", image: Rajvi, year: "Senior", major: "CS + Linguistics", linkedin: "https://www.linkedin.com/in/rajvi-shah12/" },
+  { name: "Rawisara Hameyotin", title: "Experience", image: MJ, year: "Junior", major: "Data Science", linkedin: "https://www.linkedin.com/in/rawisarahameyotin/" },
+  { name: "Adithya Prasad", title: "Experience", image: Adithya, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/adithya-prasad16/" },
+  { name: "Rasleen Dhaliwal", title: "Experience", image: Rasleen, year: "Senior", major: "CS + Design", linkedin: "https://www.linkedin.com/in/rasleen-dhaliwal-/" },
+  { name: "Dominic Irla", title: "Web Development", image: Dominic, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/dominic-irla/" },
+  { name: "Josephine Lee", title: "Web Development", image: Josephine, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/josephine-b-l/" },
+  { name: "Daniel Barajas", title: "Web Development", image: Daniel, year: "Senior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/danbarajas/" },
+  { name: "Fatima Rasheed", title: "Web Development", image: Fatima, year: "Junior", major: "Computer Science", linkedin: "https://www.linkedin.com/in/fatima-rasheed-/" },
+];
---
+++ src/data/teamPhotos.ts
@@ -0,0 +1,15 @@
+import Director from "../assets/teams/director.webp";
+import Logistics from "../assets/teams/logistics.webp";
+import Experience from "../assets/teams/experience.webp";
+import WebDevelopment from "../assets/teams/web_dev.webp";
+import Communications from "../assets/teams/communications.webp";
+import Outreach from "../assets/teams/outreach.webp";
+
+export const teamPhotos = {
+  'Director': Director,
+  'Logistics': Logistics,
+  'Experience': Experience,
+  'Web Development': WebDevelopment,
+  'Communications': Communications,
+  'Outreach': Outreach
+};
